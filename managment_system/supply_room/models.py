8dfed6d3caa2b239f795b4fe0604880e9dd6597d
christabel-alvarado <christabel.alvarado@gmail.com>
2024-05-20 23:46:29 -0600

    Create Django project and database

---
+++ managment_system/supply_room/models.py
@@ -0,0 +1,51 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+
+# specifying choices
+
+STATUS_CHOICES = (
+    ("requested", "Solicitado"),
+    ("lent", "Prestado"),
+    ("returned", "Devuelto"),
+    ("denied", "Denegado"),
+)
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+
+class Users(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    role = models.CharField(max_length=200)
+    student_id = models.CharField(max_length=200)
+
+class Class(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=200)
+class Groups(models.Model):
+    number = models.PositiveIntegerField()
+    semester = models.CharField(max_length=200)
+    professor = models.ForeignKey(Users, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
+
+class ItemOrder(models.Model):
+    status = models.CharField(
+        max_length = 20,
+        choices = STATUS_CHOICES,
+        default = 'requested'
+        )
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    code = models.CharField(max_length=200)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+class UserOrder(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+class StudentGroups(models.Model):
+    student = models.ForeignKey(Users, on_delete=models.CASCADE)
+    group = models.ForeignKey(Groups, on_delete=models.CASCADE)
