d787fd179be3ade7ca1ea03b91bb6f754ae11e5b
christabel-alvarado <christabel.alvarado@gmail.com>
2024-05-26 16:05:37 -0600

    fix error I don't get, templates/pages does not work, but templates/anythingelsethatisnotpages does

--- managment_system/managment_system/settings.py
+++ managment_system/managment_system/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
   BASE_DIR / "static",
-  BASE_DIR.parent / "node_modules",
+  BASE_DIR / "node_modules",
 ]
 
 STATIC_ROOT = BASE_DIR.parent / "static"
