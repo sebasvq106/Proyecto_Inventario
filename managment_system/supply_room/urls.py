13f40e2b882397a0fb09f589902d059a193563c4
christabel-alvarado <christabel.alvarado@gmail.com>
2024-05-27 16:31:30 -0600

    add cursos

--- managment_system/supply_room/urls.py
+++ managment_system/supply_room/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 # importing views from views.py
-from .views import ItemList, ItemCreate
-from django.urls import reverse
+from .views import ItemList, ItemCreate, ClassList, ClassCreate
 
 urlpatterns = [
 	path('articulos', ItemList.as_view(template_name='page/articulos.html'), name='articulos'),
 	path('crear-articulo', ItemCreate.as_view(template_name='page/crear-articulo.html', success_url='articulos'), name='crear-articulo'),
+	path('cursos', ClassList.as_view(template_name='page/cursos.html'), name='cursos'),
+	path('crear-curso', ClassCreate.as_view(template_name='page/crear-curso.html', success_url='cursos'), name='crear-curso'),
 ]
--- managment_system/supply_room/views.py
+++ managment_system/supply_room/views.py
@@ -3,7 +3,8 @@ from django.views.generic import CreateView
 
 # Create your views here.
 from django.views.generic.list import ListView
-from .models import Item
+from .models import Item, Class
+
 
 class ItemList(ListView):
 
@@ -23,3 +24,22 @@ class ItemCreate(CreateView):
 
 	# specify the fields to be displayed
 	fields = ['name']
+
+
+class ClassList(ListView):
+
+	# specify the model for list view
+	model = Class
+	paginate_by = 10
+
+	def get_queryset(self, *args, **kwargs):
+		qs = super(ClassList, self).get_queryset(*args, **kwargs)
+		qs = qs.order_by('name')
+		return qs
+
+class ClassCreate(CreateView):
+	# specify the model for create view
+	model = Class
+
+	# specify the fields to be displayed
+	fields = ['name', 'code']
